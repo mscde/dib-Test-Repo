2059bb984a701aad71793bb68a2e3cc16893b849
msc Coding Development Engineering <115454023+mscde@users.noreply.github.com>
2025-08-03 20:04:44 +0200

    #1 Die Variable B wurde hiunzugefügt

--- Hello_World.py
+++ Hello_World.py
@@ -4,3 +4,7 @@ print("my GIT Repo Test File")
 # 2. Add new Code in the file
 a = 5
 print(f"\nDie Variable a hat den Wert: {a}")
+
+# 3. Issue B hinzufügen
+b = 8
+print(f"\nDie Variable b hat den Wert: {b}")
